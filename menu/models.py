b80836cc0f09f9f648275e7d9761dfc6c5ebab62
Achyut Neupane <55221492+achyutneupane@users.noreply.github.com>
2020-08-08 17:06:17 +0545

    Update models.py

--- menu/models.py
+++ menu/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
-from ckeditor.fields import RichTextField
 
 # Create your models here.
 class MainMenu(models.Model):
